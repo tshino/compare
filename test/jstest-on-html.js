6b5c868de3a43c7a5bfd070d7e2656ad63ab2fb2
tshino <tshino.ssr@gmail.com>
2017-12-16 01:52:42 +0900

    Add async test support #34

--- test/jstest-on-html.js
+++ test/jstest-on-html.js
@@ -34,7 +34,7 @@
         }
       }
       for (var i = 0; i < stack.length; i += 1) {
-        if (null !== stack[i].match(/\bRUN_ALL_TESTS\b/)) {
+        if (null !== stack[i].match(/\b(START_TESTS|CONTINUE_TESTS)\b/)) {
           stack.splice(i);
           break;
         }
@@ -45,41 +45,73 @@
       log('error', '#' + errorCount + ' Test Failed:' + stack);
     }
   };
-  var RUN_ALL_TESTS = function() {
-    log('info', 'TEST STARTED!');
-    var initialLoop = true;
-    for (;;) {
+  var initialLoop = true;
+  var loopStarted = false;
+  var loopCount = 0;
+  var waitingDoneID = 0;
+  var CONTINUE_TESTS = function() {
+    waitingDoneID += 1;
+    if (!loopStarted) {
+      loopStarted = true;
       fallback = false;
-      for (var i = 0; i < testFunctions.length; ++i) {
-        var test = testFunctions[i];
-        if (initialLoop) {
-          log('section', test.name + '...');
-        }
-        var func = test.func;
+      loopCount = 0;
+    }
+    for (; loopCount < testFunctions.length; loopCount += 1) {
+      var test = testFunctions[loopCount];
+      if (initialLoop) {
+        log('section', test.name + '...');
+      }
+      var func = test.func;
+      if (func.length === 0) {
         func();
+      } else {
+        var done = CONTINUE_TESTS;
+        loopCount += 1;
+        var doneID = waitingDoneID;
+        func(done);
+        var timeout = 2000;
+        window.setTimeout(function() {
+          if (doneID === waitingDoneID) {
+            errorCount += 1;
+            log('error', '#' + errorCount + ' Timeout ' + timeout + 'msec');
+            CONTINUE_TESTS();
+            return;
+          }
+        }, timeout);
+        return;
       }
-      if (fallback) {
-        EXPECT = console.assert;
-        initialLoop = false;
-        continue;
+    }
+    loopStarted = false;
+    if (initialLoop) {
+      initialLoop = false;
+      if (errorCount === 0) {
+        log('green', testCount + ' TESTS PASSED!  (no error)');
+      } else {
+        log('red', errorCount + ' TESTS FAILED!  (' + (testCount - errorCount) + ' tests passed)');
       }
-      break;
     }
-    if (errorCount === 0) {
-      log('green', testCount + ' TESTS PASSED!  (no error)');
-    } else {
-      log('red', errorCount + ' TESTS FAILED!  (' + (testCount - errorCount) + ' tests passed)');
+    if (fallback) {
+      EXPECT = console.assert;
+      CONTINUE_TESTS();
+      return;
     }
+  };
+  var START_TESTS = function() {
+    log('info', 'TEST STARTED!');
+    initialLoop = true;
+    loopStarted = false;
+    CONTINUE_TESTS();
+    return;
   };
   return {
     log: log,
     defineTest: defineTest,
     expect: EXPECT_IMPL,
-    runAllTests: RUN_ALL_TESTS
+    runAllTests: START_TESTS
   };
 })();
 
 var LOG = jsTestOnHTML.log;
 var TEST = jsTestOnHTML.defineTest;
 var EXPECT = jsTestOnHTML.expect;
-var RUN_ALL_TESTS = jsTestOnHTML.runAllTests;
+var START_TESTS = jsTestOnHTML.runAllTests;
