37e77f01ff089cc6fa97085bac031b455f5f1043
tshino <tshino.ssr@gmail.com>
2017-12-20 02:02:31 +0900

    Add self test to jstest-util #34

--- test/jstest-util.js
+++ test/jstest-util.js
@@ -1,4 +1,5 @@
 ﻿var jsTestUtil = (function() {
+  var readFileUnavailable = false;
   var readFile = function(url, callbacks) {
     var onsuccess = callbacks.onsuccess || function() {};
     var onfail = callbacks.onfail || function() {};
@@ -32,6 +33,33 @@
     ).join('');
     return 'data:application/octet-stream;base64,' + btoa(str);
   };
+  var makeSelfTest = function(done) {
+    var run = function() {
+      readFile('data/hello.txt', {
+        onsuccess: function(response) {
+          var view = new Uint8Array(response);
+          EXPECT_EQ( 14, view.length );
+          EXPECT_EQ( 72 /* H */, view[0] );
+          EXPECT_EQ( 33 /* ! */, view[12] );
+          EXPECT_EQ( 10 /* \n */, view[13] );
+          var datauri = dataURIFromArrayBuffer(response);
+          var binary = compareUtil.binaryFromDataURI(datauri);
+          EXPECT_EQ( 14, binary.length );
+          EXPECT_EQ( 72 /* H */, binary.at(0) );
+          EXPECT_EQ( 33 /* ! */, binary.at(12) );
+          EXPECT_EQ( 10 /* \n */, binary.at(13) );
+          done();
+        },
+        onfail: function(message) {
+          WARN(message);
+          WARN('Since XHR seems to be unavailable, all tests using data files will be skipped.');
+          readFileUnavailable = true;
+          done();
+        }
+      });
+    };
+    return { run: run };
+  };
   var makeFileBasedTestRunner = function(done) {
     var totalXHR = 0;
     var doneXHR = 0;
@@ -43,6 +71,9 @@
       }
     };
     var readFileAndTest = function(url, callback) {
+      if (readFileUnavailable) {
+        return;
+      }
       totalXHR += 1;
       readFile(url, {
         onsuccess : function(response) {
@@ -60,6 +91,11 @@
       });
     };
     var run = function() {
+      if (readFileUnavailable) {
+        WARN('skipped');
+        done();
+        return;
+      }
       startedXHR = true;
       checkFinish();
     };
@@ -71,6 +107,7 @@
   return {
     readFile: readFile,
     dataURIFromArrayBuffer: dataURIFromArrayBuffer,
+    makeSelfTest: makeSelfTest,
     makeFileBasedTestRunner: makeFileBasedTestRunner
   };
 })();
