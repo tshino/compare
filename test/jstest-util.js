587e23ccedb838164ba287fef3a5c6dcc1485616
tshino <tshino.ssr@gmail.com>
2017-12-22 02:51:42 +0900

    Add makeSequentialTest() to jstest-util.js #34

--- test/jstest-util.js
+++ test/jstest-util.js
@@ -1,4 +1,22 @@
 ﻿var jsTestUtil = (function() {
+  var makeSequentialTest = function(tests) {
+    return function(done) {
+      var runRest = done;
+      for (var i = tests.length; i-- > 0; ) {
+        runRest = (function(func, runRest) {
+          return function() {
+            if (func.length === 0) {
+              func();
+              runRest();
+            } else {
+              func(runRest);
+            }
+          };
+        })(tests[i], runRest);
+      }
+      runRest();
+    };
+  };
   var readFileUnavailable = false;
   var readFile = function(url, callbacks) {
     var onsuccess = callbacks.onsuccess || function() {};
@@ -34,7 +52,7 @@
     return 'data:application/octet-stream;base64,' + btoa(str);
   };
   var makeSelfTest = function() {
-    var run = function(done) {
+    var readFileTest = function(done) {
       readFile('data/hello.txt', {
         onsuccess: function(response) {
           var view = new Uint8Array(response);
@@ -42,12 +60,6 @@
           EXPECT_EQ( 72 /* H */, view[0] );
           EXPECT_EQ( 33 /* ! */, view[12] );
           EXPECT_EQ( 10 /* \n */, view[13] );
-          var datauri = dataURIFromArrayBuffer(response);
-          var binary = compareUtil.binaryFromDataURI(datauri);
-          EXPECT_EQ( 14, binary.length );
-          EXPECT_EQ( 72 /* H */, binary.at(0) );
-          EXPECT_EQ( 33 /* ! */, binary.at(12) );
-          EXPECT_EQ( 10 /* \n */, binary.at(13) );
           done();
         },
         onfail: function(message) {
@@ -58,6 +70,31 @@
         }
       });
     };
+    var dataURIFromArrayBufferTest = function() {
+      var ab = new ArrayBuffer(14);
+      var u8 = new Uint8Array(ab);
+      // 'Hello, world!\n'
+      u8[0] = 72;
+      u8[1] = 101;
+      u8[2] = 108;
+      u8[3] = 108;
+      u8[4] = 111;
+      u8[5] = 44;
+      u8[6] = 32;
+      u8[7] = 119;
+      u8[8] = 111;
+      u8[9] = 114;
+      u8[10] = 108;
+      u8[11] = 100;
+      u8[12] = 33;
+      u8[13] = 10;
+      var datauri = dataURIFromArrayBuffer(ab);
+      EXPECT_EQ( 'data:application/octet-stream;base64,SGVsbG8sIHdvcmxkIQo=', datauri );
+    };
+    var run = makeSequentialTest([
+      readFileTest,
+      dataURIFromArrayBufferTest,
+    ]);
     return { run: run };
   };
   var makeFileBasedTestRunner = function() {
@@ -102,6 +139,7 @@
     };
   };
   return {
+    makeSequentialTest: makeSequentialTest,
     readFile: readFile,
     dataURIFromArrayBuffer: dataURIFromArrayBuffer,
     makeSelfTest: makeSelfTest,
