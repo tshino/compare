074cc87ce72a9aedabb49f9ccb57076ea8f1567e
tshino <tshino.ssr@gmail.com>
2018-01-09 02:07:33 +0900

    Add test for compare-image-util.js #35

---
+++ test/compare-image-util-test.js
@@ -0,0 +1,23 @@
+﻿TEST( 'compareImageUtil makeImage', function test() {
+  var image = compareImageUtil.makeImage(300, 200);
+  EXPECT_EQ( 300, image.width );
+  EXPECT_EQ( 200, image.height );
+  EXPECT_EQ( 300, image.pitch );
+  EXPECT_EQ( 240000, image.data.length );
+  EXPECT_EQ( 0, image.offset );
+
+  var image2 = compareImageUtil.makeImage(image);
+  EXPECT_EQ( 300, image2.width );
+  EXPECT_EQ( 200, image2.height );
+  EXPECT_EQ( 300, image2.pitch );
+  EXPECT_EQ( image.data, image2.data );
+  EXPECT_EQ( 0, image2.offset );
+
+  var imageData = { width: 300, height: 200, data: new Uint8Array(240000) };
+  var image3 = compareImageUtil.makeImage(imageData);
+  EXPECT_EQ( 300, image3.width );
+  EXPECT_EQ( 200, image3.height );
+  EXPECT_EQ( 300, image3.pitch );
+  EXPECT_EQ( imageData.data, image3.data );
+  EXPECT_EQ( 0, image3.offset );
+});
