f0fc8a5103e7fba5cae2e5e8a132eaf637dc2c27
tshino <tshino.ssr@gmail.com>
2018-04-11 01:49:22 +0900

    Add more tests for calcMetrics() #35

--- test/image-calculation-test.js
+++ test/image-calculation-test.js
@@ -84,62 +84,102 @@
     ])(done);
   });
   TEST( 'compare-worker.js calcMetrics', function test(done) {
+    var colorImage1 = {
+      width: 4,
+      height: 4,
+      data: [
+        0, 0, 0, 255,  0, 0, 64, 255,  0, 0, 128, 255,  0, 0, 192, 255,
+        0, 1, 0, 255,  0, 1, 64, 255,  0, 1, 128, 255,  0, 1, 192, 255,
+        0, 1, 0, 255,  0, 1, 64, 255,  0, 1, 128, 255,  0, 1, 192, 255,
+        0, 1, 0, 255,  0, 1, 64, 255,  0, 1, 128, 255,  0, 1, 192, 255,
+      ]
+    };
+    // different with colorImage1 in only red component of every pixel
+    var colorImage2 = {
+      width: 4,
+      height: 4,
+      data: [
+        30, 0, 0, 255,  30, 0, 64, 255,  30, 0, 128, 255,  30, 0, 192, 255,
+        30, 1, 0, 255,  30, 1, 64, 255,  30, 1, 128, 255,  30, 1, 192, 255,
+        30, 1, 0, 255,  30, 1, 64, 255,  30, 1, 128, 255,  30, 1, 192, 255,
+        30, 1, 0, 255,  30, 1, 64, 255,  30, 1, 128, 255,  30, 1, 192, 255,
+      ]
+    };
+    var blackImage = {
+      width: 4,
+      height: 4,
+      data: [
+        0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255,
+        0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255,
+        0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255,
+        0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255
+      ]
+    };
+    var whiteImage = {
+      width: 4,
+      height: 4,
+      data: [
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255
+      ]
+    }
     jsTestUtil.makeSequentialTest([
+      // exactly same images
       function(done) {
         var task = {
           cmd: 'calcMetrics',
-          imageData: [{
-            width: 4,
-            height: 4,
-            data: [
-              0, 0, 0, 255,  0, 0, 64, 255,  0, 0, 128, 255,  0, 0, 192, 255,
-              0, 0, 0, 255,  0, 0, 64, 255,  0, 0, 128, 255,  0, 0, 192, 255,
-              0, 1, 0, 255,  0, 1, 64, 255,  0, 1, 128, 255,  0, 1, 192, 255,
-              0, 1, 0, 255,  0, 1, 64, 255,  0, 1, 128, 255,  0, 1, 192, 255,
-            ]
-          },{
-            width: 4,
-            height: 4,
-            data: [
-              0, 0, 0, 255,  0, 0, 64, 255,  0, 0, 128, 255,  0, 0, 192, 255,
-              0, 0, 0, 255,  0, 0, 64, 255,  0, 0, 128, 255,  0, 0, 192, 255,
-              0, 1, 0, 255,  0, 1, 64, 255,  0, 1, 128, 255,  0, 1, 192, 255,
-              0, 1, 0, 255,  0, 1, 64, 255,  0, 1, 128, 255,  0, 1, 192, 255,
-            ]
-          }]
+          imageData: [colorImage1, colorImage1]
         };
         taskCallback = function(data) {
           EXPECT_EQ( 'calcMetrics', data.cmd );
           EXPECT_EQ( Infinity, data.result.psnr );
           EXPECT_EQ( 0, data.result.mse );
-          EXPECT_EQ( 1, data.result.ncc );
+          //EXPECT_EQ( 1, data.result.ncc );
           EXPECT_EQ( 0, data.result.ae );
           done();
         };
         taskQueue.addTask(task);
       },
+      // different images
       function(done) {
         var task = {
           cmd: 'calcMetrics',
-          imageData: [{
-            width: 4,
-            height: 4,
-            data: [
-              0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255,
-              0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255,
-              0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255,
-              0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255
-            ]
-          },{
-            width: 4,
-            height: 4,
-            data: [
-              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255
-            ]
-          }]
+          imageData: [colorImage1, colorImage2]
+        };
+        taskCallback = function(data) {
+          EXPECT_EQ( 'calcMetrics', data.cmd );
+          EXPECT( 1e-14 > Math.abs(10 * Math.log10((3*255*255) / (30*30)) - data.result.psnr) );
+          EXPECT_EQ( (30*30)/3, data.result.mse );
+          //EXPECT_EQ( ????, data.result.ncc );
+          EXPECT_EQ( 16, data.result.ae );
+          done();
+        };
+        taskQueue.addTask(task);
+      },
+      // exactly same flat images
+      // Note: NCC between flat image and any image is always 0 (definition in this app)
+      function(done) {
+        var task = {
+          cmd: 'calcMetrics',
+          imageData: [blackImage, blackImage]
+        };
+        taskCallback = function(data) {
+          EXPECT_EQ( 'calcMetrics', data.cmd );
+          EXPECT_EQ( Infinity, data.result.psnr );
+          EXPECT_EQ( 0, data.result.mse );
+          EXPECT_EQ( 0, data.result.ncc );
+          EXPECT_EQ( 0, data.result.ae );
+          done();
+        };
+        taskQueue.addTask(task);
+      },
+      // different flat images
+      function(done) {
+        var task = {
+          cmd: 'calcMetrics',
+          imageData: [blackImage, whiteImage]
         };
         taskCallback = function(data) {
           EXPECT_EQ( 'calcMetrics', data.cmd );
@@ -147,6 +187,22 @@
           EXPECT_EQ( 255 * 255, data.result.mse );
           EXPECT_EQ( 0, data.result.ncc );
           EXPECT_EQ( 16, data.result.ae );
+          done();
+        };
+        taskQueue.addTask(task);
+      },
+      // different images, one is flat
+      function(done) {
+        var task = {
+          cmd: 'calcMetrics',
+          imageData: [blackImage, colorImage1]
+        };
+        taskCallback = function(data) {
+          EXPECT_EQ( 'calcMetrics', data.cmd );
+          //EXPECT_EQ( ???, data.result.psnr );
+          //EXPECT_EQ( ???, data.result.mse );
+          EXPECT_EQ( 0, data.result.ncc );
+          EXPECT_EQ( 15, data.result.ae );
           done();
         };
         taskQueue.addTask(task);
