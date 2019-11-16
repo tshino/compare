c4ec4496bd420f3385a3ad36267c62bc317bc920
tshino <tshino.ssr@gmail.com>
2019-11-16 18:01:42 +0900

    Add tests for animated WebP format detection #103#

--- test/compare-util-test.js
+++ test/compare-util-test.js
@@ -1560,6 +1560,29 @@ TEST( 'compareUtil findNearlyConstantValue', function test() {
     EXPECT_EQ( 1000, f.anim && f.anim.fpsNum );
     EXPECT_EQ( 70, f.anim && f.anim.fpsDen );
     EXPECT( f.anim && f.anim.approxFPS && 0.05 >= Math.abs(100/7 - f.anim.approxFPS) );
+
+    var f = detect([
+      0x52, 0x49, 0x46, 0x46, 0,0,0,0, 0x57, 0x45, 0x42, 0x50,
+      0x56, 0x50, 0x38, 0x58, 0x0a, 0, 0, 0, // 'VP8X'
+      0x02, 0x00, 0x00, 0x00, 0, 0, 0, 0, 0, 0,
+      0x41, 0x4E, 0x4D, 0x46, 43, 0, 0, 0, // 'ANMF'
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 70, 0, 0, 0,
+      0x41, 0x4C, 0x50, 0x48, 0, 0, 0, 0, // 'ALPH'
+      0x56, 0x50, 0x38, 0x20, 11, 0, 0, 0, // 'VP8 '
+      0, 0, 0, 0x9d, 0x01, 0x2a, 0, 0, 0, 0, 0x00, 99,
+      0x41, 0x4E, 0x4D, 0x46, 29, 0, 0, 0, // 'ANMF'
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 70, 0, 0, 0,
+      0x56, 0x50, 0x38, 0x4c, 5, 0, 0, 0, // 'VP8L'
+      0x2f, 0, 0, 0, 0x10, 99,
+    ]);
+    EXPECT_EQ( 'WebP (Animated Lossy+Lossless)', f.toString() );
+    EXPECT_EQ( 'RGBA 8.8.8.8 (32bpp), YCbCr 8.8.8 (12bpp 4:2:0) + Alpha 8', f.color );
+    EXPECT_EQ( 2, f.anim && f.anim.frameCount );
+    EXPECT_EQ( 140, f.anim && f.anim.durationNum );
+    EXPECT_EQ( 1000, f.anim && f.anim.durationDen );
+    EXPECT_EQ( 1000, f.anim && f.anim.fpsNum );
+    EXPECT_EQ( 70, f.anim && f.anim.fpsDen );
+    EXPECT( f.anim && f.anim.approxFPS && 0.05 >= Math.abs(100/7 - f.anim.approxFPS) );
   });
 
   TEST( 'compareUtil detectImageFormat SVG', function test() {
