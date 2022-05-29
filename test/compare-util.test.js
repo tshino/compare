9066987825b294fc1519abc59ac4fdd1ff0324f0
tshino <tshino.ssr@gmail.com>
2022-05-29 13:08:29 +0900

    Migrate tests to Mocha #110

--- test/compare-util.test.js
+++ test/compare-util.test.js
@@ -123,4 +123,113 @@ describe('CompareUtil', () => {
             assert.strictEqual(compareUtil.toPercent(1), '100%');
         });
     });
+
+    describe('toHexTriplet', () => {
+        it('should format given RGB value into a hex triplet', () => {
+            assert.strictEqual(compareUtil.toHexTriplet(0, 0, 0), '#000000');
+            assert.strictEqual(compareUtil.toHexTriplet(1, 1, 1), '#010101');
+            assert.strictEqual(compareUtil.toHexTriplet(255, 255, 255), '#FFFFFF');
+            assert.strictEqual(compareUtil.toHexTriplet(0, 0, 255), '#0000FF');
+            assert.strictEqual(compareUtil.toHexTriplet(0, 255, 0), '#00FF00');
+            assert.strictEqual(compareUtil.toHexTriplet(255, 0, 0), '#FF0000');
+            assert.strictEqual(compareUtil.toHexTriplet(85, 170, 255), '#55AAFF');
+            assert.strictEqual(compareUtil.toHexTriplet(18, 52, 86), '#123456');
+
+            assert.strictEqual(compareUtil.toHexTriplet(-1, -2, -3), '#000000');
+            assert.strictEqual(compareUtil.toHexTriplet(257, 300, 1000), '#FFFFFF');
+            assert.strictEqual(compareUtil.toHexTriplet(15.9, 16, 16.1), '#101010');
+        });
+    });
+
+    describe('srgb255ToLinear255', () => {
+        it('should convert sRGB 8bit int value to linear RGB float value [0,255]', () => {
+            assert.strictEqual(compareUtil.srgb255ToLinear255[0], 0.0);
+            assert.ok(1e-5 > Math.abs(0.077399385 - compareUtil.srgb255ToLinear255[1]));
+            assert.ok(1e-5 > Math.abs(0.386996900 - compareUtil.srgb255ToLinear255[5]));
+            assert.ok(1e-5 > Math.abs(0.773993799 - compareUtil.srgb255ToLinear255[10]));
+            assert.ok(1e-5 > Math.abs(0.853366629 - compareUtil.srgb255ToLinear255[11]));
+            assert.ok(1e-5 > Math.abs(1.218123143 - compareUtil.srgb255ToLinear255[15]));
+            assert.ok(1e-5 > Math.abs(55.044427526 - compareUtil.srgb255ToLinear255[128]));
+            assert.ok(1e-5 > Math.abs(134.414357054 - compareUtil.srgb255ToLinear255[192]));
+            assert.ok(1e-5 > Math.abs(252.731034761 - compareUtil.srgb255ToLinear255[254]));
+            assert.strictEqual(compareUtil.srgb255ToLinear255[255], 255.0);
+        });
+    });
+
+    describe('srgb255ToLinear8', () => {
+        it('should convert sRGB 8bit int value to linear RGB 8bit int value', () => {
+            assert.strictEqual(compareUtil.srgb255ToLinear8[0], 0);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[1], 0);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[5], 0);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[10], 1);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[11], 1);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[15], 1);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[128], 55);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[192], 134);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[254], 253);
+            assert.strictEqual(compareUtil.srgb255ToLinear8[255], 255);
+        });
+    });
+
+    describe('convertColorListRgbToLinear', () => {
+        const rgb = [
+            0x000000, 0x808080, 0xffffff, 0xff0000, 0x00ff00, 0x0000ff,
+            0x010203, 0x090a0b, 0x112233, 0x445566, 0x778899, 0xbbddff
+        ];
+        const expected = [
+            0x000000, 0x373737, 0xffffff, 0xff0000, 0x00ff00, 0x0000ff,
+            0x000000, 0x010101, 0x010408, 0x0f1722, 0x2f3f51, 0x7fb8ff
+        ];
+        it('should convert sRGB 888 int values to linear RGB 888 int values', () => {
+            const linear = compareUtil.convertColorListRgbToLinear(rgb);
+            assert.strictEqual(linear.length, rgb.length);
+            assert.deepStrictEqual(Array.from(linear), expected);
+        });
+    });
+
+    describe('convertColorListRgbToXyy', () => {
+        const rgb = [
+            0x000000, 0x808080, 0xffffff,
+            0xff0000, 0x00ff00, 0x0000ff,
+            0x800000, 0x008000, 0x000080
+        ];
+        it('should convert sRGB 888 int values to Xyy 888 int values', () => {
+            const xyy = compareUtil.convertColorListRgbToXyy(rgb);
+            assert.strictEqual(xyy.length, rgb.length);
+            assert.strictEqual(xyy[0], 0x787e00); // x=79.74, y=83.90, Y = 0
+            assert.strictEqual(xyy[1], 0x787e37); // x=79.74, y=83.90, Y = 55.04
+            assert.strictEqual(xyy[2], 0x787eff); // x=79.74, y=83.90, Y = 255
+            assert.strictEqual(xyy[3], 0xf57e36); // x=163.20, y=84.15, Y = 54.23
+            assert.strictEqual(xyy[4], 0x73e6b6); // x=76.50, y=153.00, Y = 182.36
+            assert.strictEqual(xyy[5], 0x391712); // x=38.25, y=15.30, Y = 18.40
+            assert.strictEqual(xyy[6], 0xf57e0c); // x=163.20, y=84.15, Y = 11.71
+            assert.strictEqual(xyy[7], 0x73e627); // x=76.50, y=153.00, Y = 39.36
+            assert.strictEqual(xyy[8], 0x391704); // x=38.25, y=15.30, Y = 3.97
+        });
+    });
+
+    describe('convertColorListRgbToHsv', () => {
+        const rgb = [
+            0x000000, 0x808080, 0xffffff,
+            0xff0000, 0x00ff00, 0x0000ff,
+            0x800000, 0x008000, 0x000080,
+            0xff8080, 0x80ff80, 0x8080ff
+        ];
+        it('should convert RGB 888 int values to HSV 888 int values', () => {
+            const hsv = compareUtil.convertColorListRgbToHsv(rgb);
+            assert.strictEqual(hsv.length, rgb.length);
+            assert.strictEqual(hsv[0], 0x808000); // H=0, S=0, V=0
+            assert.strictEqual(hsv[1], 0x808080); // H=0, S=0, V=50.2
+            assert.strictEqual(hsv[2], 0x8080ff); // H=0, S=0, V=100
+            assert.strictEqual(hsv[3], 0xff80ff); // H=0, S=100, V=100
+            assert.strictEqual(hsv[4], 0x40eeff); // H=120, S=100, V=100
+            assert.strictEqual(hsv[5], 0x4011ff); // H=240, S=100, V=100
+            assert.strictEqual(hsv[6], 0xff8080); // H=0, S=100, V=50.2
+            assert.strictEqual(hsv[7], 0x40ee80); // H=120, S=100, V=50.2
+            assert.strictEqual(hsv[8], 0x401180); // H=240, S=100, V=50.2
+            assert.strictEqual(hsv[9], 0xbf80ff); // H=0, S=49.2, V=100
+            assert.strictEqual(hsv[10], 0x60b6ff); // H=120, S=49.2, V=100
+            assert.strictEqual(hsv[11], 0x6049ff); // H=240, S=49.2, V=100
+        });
+    });
 });
