045d701ef4361d840caf8f2f8f4876f726d810e0
tshino <tshino.ssr@gmail.com>
2022-06-04 17:03:46 +0900

    Migrate tests to Mocha #110

--- test/compare-image-util.test.js
+++ test/compare-image-util.test.js
@@ -12,4 +12,26 @@ describe('CompareImageUtil', () => {
             assert.strictEqual(channelsOf(null), 4);
         });
     });
+
+    describe('newArrayOf', () => {
+        const newArrayOf = compareImageUtil.newArrayOf;
+        it('should allocate a new array for an image', () => {
+            const u8_0 = newArrayOf(compareImageUtil.FORMAT_U8x4, 0);
+            assert.strictEqual(u8_0.length, 0)
+            const u8_1 = newArrayOf(compareImageUtil.FORMAT_U8x4, 1);
+            assert.strictEqual(u8_1.length, 1)
+            assert.strictEqual(typeof u8_1[0], 'number')
+            const u8_100 = newArrayOf(compareImageUtil.FORMAT_U8x4, 100);
+            assert.strictEqual(u8_100.length, 100)
+            assert.strictEqual(typeof u8_100[0], 'number')
+            const f8_0 = newArrayOf(compareImageUtil.FORMAT_F32x1, 0);
+            assert.strictEqual(f8_0.length, 0)
+            const f8_1 = newArrayOf(compareImageUtil.FORMAT_F32x1, 1);
+            assert.strictEqual(f8_1.length, 1)
+            assert.strictEqual(typeof f8_1[0], 'number')
+            const f8_100 = newArrayOf(compareImageUtil.FORMAT_F32x1, 100);
+            assert.strictEqual(f8_100.length, 100)
+            assert.strictEqual(typeof f8_100[0], 'number')
+        });
+    });
 });
