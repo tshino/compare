cd5498e502f7fe32a3cd21fc61c5e6ba04debee7
tshino <tshino.ssr@gmail.com>
2022-12-04 17:50:04 +0900

    Refactor

--- modules/compare-ui.js
+++ modules/compare-ui.js
@@ -311,6 +311,311 @@ const CompareUI = function({ compareUtil }) {
         };
     };
 
+    const CrossCursor = function ({ view }) {
+        const viewZoom = view.viewZoom;
+        let enableCrossCursor = false;
+        let primaryIndex = null;
+        let fixedPosition = false;
+        const positions = [];
+        const onShowCallback = [];
+        const onUpdateCallback = [];
+        const onRemoveCallback = [];
+
+        const makeInitialPosition = function (index) {
+            const img = view.getEntry(index);
+            const center = viewZoom.getCenter();
+            const x = (0.5 + center.x) * img.width;
+            const y = (0.5 + center.y) * img.height;
+            return { x, y };
+        };
+        const addObserver = function (onShow, onUpdate, onRemove) {
+            if (onShow) {
+                onShowCallback.push(onShow);
+            }
+            if (onUpdate) {
+                onUpdateCallback.push(onUpdate);
+            }
+            if (onRemove) {
+                onRemoveCallback.push(onRemove);
+            }
+        };
+        const enable = function () {
+            const index = view.getCurrentIndexOr(view.getFrontIndex());
+            if (!enableCrossCursor && index !== null) {
+                enableCrossCursor = true;
+                primaryIndex = index;
+                fixedPosition = false;
+                onShowCallback.forEach(function (val) { val(); });
+                const pos = makeInitialPosition(index);
+                setPosition(index, pos.x, pos.y);
+                view.updateLayout();
+            }
+            return enableCrossCursor;
+        };
+        const disable = function () {
+            if (enableCrossCursor) {
+                enableCrossCursor = false;
+                onRemoveCallback.forEach(function (val) { val(); });
+                primaryIndex = null;
+                view.updateLayout();
+            }
+        };
+        const toggle = function () {
+            if (!enableCrossCursor) {
+                enable();
+            } else {
+                disable();
+            }
+        };
+        const getPosition = function (index) {
+            index = index !== undefined ? index : primaryIndex;
+            return positions[index];
+        };
+        const setIndex = function (index, fixed) {
+            primaryIndex = index;
+            fixedPosition = fixed;
+        };
+        const getIndex = function () {
+            return primaryIndex;
+        };
+        const getNormalizedPosition = function () {
+            const entry = view.getEntry(primaryIndex);
+            return {
+                x: (0.5 + positions[primaryIndex].x) / entry.width,
+                y: (0.5 + positions[primaryIndex].y) / entry.height,
+                isInView: positions[primaryIndex].isInView
+            };
+        };
+        const isFixed = function () {
+            return fixedPosition;
+        };
+        const isInsideROI = function (roi, x, y) {
+            return (
+                roi[0] <= x && x <= roi[2] &&
+                roi[1] <= y && y <= roi[3]
+            );
+        };
+        const onRemoveEntry = function (index) {
+            if (enableCrossCursor && primaryIndex === index) {
+                primaryIndex = view.findImageIndexOtherThan(index);
+                if (primaryIndex === null) {
+                    enableCrossCursor = false;
+                    onRemoveCallback.forEach(function (val) { val(); });
+                }
+            }
+        };
+        const makePathDesc = function (img, x, y) {
+            const pos = img.interpretXY(x, y);
+            let desc = '';
+            desc += 'M ' + pos.x + ',0 l 0,' + img.canvasHeight + ' ';
+            desc += 'M ' + (pos.x + 1) + ',0 l 0,' + img.canvasHeight + ' ';
+            desc += 'M 0,' + pos.y + ' l ' + img.canvasWidth + ',0 ';
+            desc += 'M 0,' + (pos.y + 1) + ' l ' + img.canvasWidth + ',0 ';
+            return desc;
+        };
+        const makeLabelAttr = function (img, roi, x, y) {
+            const attr = makeLabelAttrOnTransform(img, roi, x, y);
+            attr[0]['text-anchor'] = img.width * 0.9 < x ? 'end' : '';
+            if (compareUtil.browserName === 'msie' || compareUtil.browserName === 'edge') {
+                attr[0]['dy'] = '40%';
+                attr[1]['dy'] = img.height * 0.9 < y ? '0%' : '40%';
+            } else {
+                attr[0]['dominant-baseline'] = 'hanging';
+                attr[1]['dominant-baseline'] = img.height * 0.9 < y ? 'alphabetic' : 'hanging';
+            }
+            return attr;
+        };
+        const makeLabelAttrOnTransform = function (ent, roi, x, y) {
+            const baseScale = ent.width / (ent.baseWidth * viewZoom.scale);
+            const sx = ent.flippedX ? -1 : 1;
+            const sy = ent.flippedY ? -1 : 1;
+            const pos = ent.interpretXY2(x, y);
+            const base = ent.interpretXY2(0, 0);
+            base.x += sx * (ent.transposed ? roi[1] : roi[0]);
+            base.y += sy * (ent.transposed ? roi[0] : roi[1]);
+            const t0 = 'translate(' + pos.x + ' ' + base.y + ') ';
+            const t1 = 'translate(' + base.x + ' ' + pos.y + ') ';
+            const s = 'scale(' + baseScale * sx + ' ' + baseScale * sy + ')';
+            const m = ent.transposed ? ' matrix(0 1 1 0 0 0)' : '';
+            const a0 = { transform: t0 + s + m }, a1 = { transform: t1 + s + m };
+            return ent.transposed ? [a1, a0] : [a0, a1];
+        };
+        const addCrossCursor = function (img, desc) {
+            const size = { w: img.canvasWidth, h: img.canvasHeight };
+            const vbox = '0 0 ' + size.w + ' ' + size.h;
+            const filter_id = 'drop-shadow' + img.index;
+            const textElem = '<text filter="url(#' + filter_id + ')"></text>';
+            img.cursor = $(
+                '<svg class="imageOverlay cursor" viewBox="' + vbox + '" style="overflow:visible">' +
+                '<defs><filter id="' + filter_id + '">' +
+                '<feGaussianBlur in="SourceAlpha" result="shadow" stdDeviation="1.5"></feGaussianBlur>' +
+                '<feBlend in="SourceGraphic" in2="shadow" mode="normal"></feBlend>' +
+                '</filter></defs>' +
+                '<path stroke="black" fill="none" stroke-width="0.2" opacity="0.1" d="' + desc + '"></path>' +
+                '<path stroke="white" fill="none" stroke-width="0.1" opacity="0.6" d="' + desc + '"></path>' +
+                '<g class="labels" font-size="16" fill="white">' +
+                textElem + textElem + '</g>' +
+                '</svg>').
+                width(size.w).
+                height(size.h);
+            img.view.append(img.cursor);
+        };
+        const removeCrossCursor = function (img) {
+            if (img.cursor) {
+                $(img.cursor).remove();
+                img.cursor = null;
+            }
+        };
+        const updateCrossCursor = function (img, x, y) {
+            if (!img.element) {
+                return;
+            }
+            x = compareUtil.clamp(x, 0, img.width - 1);
+            y = compareUtil.clamp(y, 0, img.height - 1);
+            positions[img.index] = { x: x, y: y, fixed: fixedPosition };
+            const desc = makePathDesc(img, x, y);
+            const roi = img.calcROI(viewZoom.scale, viewZoom.getCenter());
+            positions[img.index].isInView = isInsideROI(roi, x, y);
+            const labelsAttr = makeLabelAttr(img, roi, x, y);
+            if (0 === img.view.find('.cursor').length) {
+                addCrossCursor(img, desc);
+            } else {
+                img.cursor.find('path').attr('d', desc);
+            }
+            img.cursor.find('path').attr('stroke-dasharray', fixedPosition ? 'none' : '4,1');
+            img.cursor.find('g.labels text').each(function (i) {
+                $(this).attr(labelsAttr[i]).text(i === 0 ? x : y);
+            });
+        };
+        const setPosition = function (index, x, y, fixed) {
+            fixed = fixed !== undefined ? fixed : fixedPosition;
+            const ent = view.getEntry(index);
+            const rx = (Math.floor(x) + 0.5) / ent.width;
+            const ry = (Math.floor(y) + 0.5) / ent.height;
+            setIndex(index, fixed);
+            for (const img of view.getImages()) {
+                const ix = compareUtil.clamp(Math.floor(rx * img.width), 0, img.width - 1);
+                const iy = compareUtil.clamp(Math.floor(ry * img.height), 0, img.height - 1);
+                updateCrossCursor(img, ix, iy);
+            }
+            onUpdateCallback.forEach(function (val) { val(true); });
+        };
+        const adjustViewOffsetToFollowCrossCursor = function (dx, dy, x, y) {
+            const img = view.getEntry(primaryIndex);
+            const center = viewZoom.getCenter();
+            const rx = (x - (0.5 + center.x) * img.width) / (img.width / viewZoom.scale);
+            const ry = (y - (0.5 + center.y) * img.height) / (img.height / viewZoom.scale);
+            if (0.45 < Math.abs(rx) && 0 < dx * rx) {
+                const delta = Math.max(0.25, Math.abs(rx) - 0.45);
+                viewZoom.moveRelative(0 < rx ? delta : -delta, 0);
+            }
+            if (0.45 < Math.abs(ry) && 0 < dy * ry) {
+                const delta = Math.max(0.25, Math.abs(ry) - 0.45);
+                viewZoom.moveRelative(0, 0 < ry ? delta : -delta);
+            }
+        };
+        const processKeyDown = function (e) {
+            if (e.ctrlKey || e.altKey || e.metaKey) {
+                return true;
+            }
+            if (enableCrossCursor) {
+                // cursor key
+                if (37 <= e.keyCode && e.keyCode <= 40) {
+                    let index = view.getCurrentIndexOr(primaryIndex);
+                    if (index < 0 || !positions[index]) {
+                        index = primaryIndex;
+                    }
+                    const step = e.shiftKey ? 10 : 1;
+                    const pos = getPosition(index);
+                    const d = compareUtil.cursorKeyCodeToXY(e.keyCode, step);
+                    const x = pos.x + d.x;
+                    const y = pos.y + d.y;
+                    setPosition(index, x, y);
+                    adjustViewOffsetToFollowCrossCursor(d.x, d.y, x, y);
+                    return false;
+                }
+            }
+        };
+        const processClick = function (e) {
+            const pos = getPosition(e.index);
+            const ent = view.getEntry(e.index);
+            const x = compareUtil.clamp(Math.floor(e.x * ent.width), 0, ent.width - 1);
+            const y = compareUtil.clamp(Math.floor(e.y * ent.height), 0, ent.height - 1);
+            let fixed;
+            if (pos && pos.x === x && pos.y === y) {
+                fixed = !pos.fixed;
+            } else {
+                fixed = true;
+            }
+            setPosition(e.index, x, y, fixed);
+        };
+        const processMouseMove = function (e, selector, target) {
+            if (enableCrossCursor && !fixedPosition) {
+                const index = selector ? $(selector).index($(target).parent()) : null;
+                const pos = viewZoom.positionFromMouseEvent(e, target, index);
+                if (view.ready(index) && pos) {
+                    const ent = view.getEntry(index);
+                    const x = pos.x * ent.width;
+                    const y = pos.y * ent.height;
+                    setPosition(index, x, y);
+                }
+            }
+        };
+        const onUpdateImageBox = function (img, w, h) {
+            if (enableCrossCursor) {
+                const pos = positions[img.index];
+                const x = pos ? (pos.x || 0) : 0;
+                const y = pos ? (pos.y || 0) : 0;
+                updateCrossCursor(img, x, y);
+            } else {
+                removeCrossCursor(img);
+            }
+            if (img.cursor) {
+                $(img.cursor).css({ width: w + 'px', height: h + 'px' });
+            }
+        };
+        const onUpdateEntryTransform = function (ent, commonStyle) {
+            if (ent.cursor) {
+                const baseScale = ent.width / (ent.baseWidth * viewZoom.scale);
+                $(ent.cursor).css(commonStyle).find('path').each(function (i) {
+                    $(this).attr('stroke-width', baseScale * [2, 1][i]);
+                });
+                const pos = positions[ent.index];
+                const roi = ent.calcROI(viewZoom.scale, viewZoom.getCenter());
+                positions[ent.index].isInView = isInsideROI(roi, pos.x, pos.y);
+                const attr = makeLabelAttrOnTransform(ent, roi, pos.x, pos.y);
+                $(ent.cursor).find('g.labels text').each(function (i) {
+                    $(this).attr(attr[i]);
+                });
+            }
+        };
+        const onUpdateTransform = function () {
+            if (enableCrossCursor) {
+                onUpdateCallback.forEach(function (val) { val(false); });
+            }
+        };
+        view.addOnRemoveEntry(onRemoveEntry);
+        view.addOnUpdateImageBox(onUpdateImageBox);
+        view.addOnUpdateEntryTransform(onUpdateEntryTransform);
+        view.addOnUpdateTransform(onUpdateTransform);
+
+        return {
+            addObserver,
+            enable,
+            disable,
+            toggle,
+            isEnabled: function () { return enableCrossCursor; },
+            getPosition,
+            getIndex,
+            getNormalizedPosition,
+            isFixed,
+            setPosition,
+            processKeyDown,
+            processClick,
+            processMouseMove
+        };
+    };
+
     const Hud = function({ view, crossCursor }) {
         const viewZoom = view.viewZoom;
         const hudPlacement = { right: true, bottom: true };
@@ -656,6 +961,7 @@ const CompareUI = function({ compareUtil }) {
         setDragStateClass,
         TextUtil,
         Grid,
+        CrossCursor,
         Hud,
         ColorHUD,
         RoiMap,
